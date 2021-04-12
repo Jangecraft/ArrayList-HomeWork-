6f9c8c1a5eaf1e74594599deff391dbd3de71463
Jangecraft <78905953+Jangecraft@users.noreply.github.com>
2021-04-12 10:36:21 +0700

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArrayListApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList MyData;
+        public MainWindow()
+        {
+            InitializeComponent();
+            MyData = new ArrayList();
+        }
+
+        private void Add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyData.Add(txtData.Text);
+            MessageBox.Show("Success");
+        }
+        private void Remove_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyData.Remove(txtData.Text);
+            MessageBox.Show("Success");
+        }
+        private void Show_Button_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (string data in MyData)
+            {
+                MessageBox.Show(data);
+            }
+        }
+    }
+}
